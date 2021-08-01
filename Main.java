65481b52e33661297f22d4ac95bd61de4748796f
uavpliyc <uavpliyc@gmail.com>
2021-08-01 10:42:33 +0900

    second commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 50;
+    int answer = x * y;
+    String greeting = "Hello World!";
+    System.out.println(greeting);
+    System.out.println(answer);
+  }
+}
