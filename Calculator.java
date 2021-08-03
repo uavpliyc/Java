a26a810d491a743a52bd4e25378575398fbef574
uavpliyc <uavpliyc@gmail.com>
2021-08-04 08:05:31 +0900

    add Calculator

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+  public static void main(String[] args) {
+    int a = 6;
+    int b = 3;
+    String x;
+    x = args[0];
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    System.out.println(a % b);
+    System.out.println(x);
+  }
+}
