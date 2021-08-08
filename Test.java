181ca1112d9bf60759dc1dc47cfcf8bef709aa98
uavpliyc <uavpliyc@gmail.com>
2021-08-08 09:22:20 +0900

    add array

--- Test.java
+++ Test.java
@@ -1,25 +1,19 @@
 public class Test {
   public static void main(String[] args) {
-  int var = 2;
-  switch(var) {
-    case 1:
-      System.out.println(1);
-      break;
-    case 2:
-      System.out.println(2);
-      // break;
-    case 3:
-      System.out.println(3);
-      // break;
-    default:
-      System.out.println("no");
-      // break;
-  }
-    // for (int i = 0; i <= 5; i++){
-    //   System.out.println(i);
-    //   if (i == 3){
-    //     continue;
-    //   }
-    // }
+    int var = 2;
+    switch(var) {
+      case 1:
+        System.out.println(1);
+        break;
+      case 2:
+        System.out.println(2);
+        break;
+      case 3:
+        System.out.println(3);
+        break;
+      default:
+        System.out.println("no");
+        break;
+    }
   }
 }
