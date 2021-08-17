9c24e15655d0e7f7f9e5e2ad7d51e2e0c57b81aa
uavpliyc <uavpliyc@gmail.com>
2021-08-18 00:33:12 +0900

    finish Deposit first

--- Deposit.java
+++ Deposit.java
@@ -1,42 +1,27 @@
 public class Deposit {
 
   //口座番号の宣言
-  private int bankNum;
-
+  private String bankNum;
   //口座名の宣言
-  private String bankName = "銀行";
-
+  private String bankName;
   //口座残高の宣言と初期化
-  private int balance = 10000;
-
+  private int balance;
 
   //コンストラクタ
-  public Deposit(){
-    
+  public Deposit(String num, String name, int iBalance){
+    bankNum = num;
+    bankName = name;
+    balance = iBalance;
   }
 
-/*
-  public Deposit(int val1, int val2, String symbol){
-    switch (symbol) {
-      case "IN":
-        money1 += val1;
-        money2 += val2;
-        break;
-      case "OUT":
-        money1 -= val1;
-        money2 -= val2;
-        break;
-    }
-  }
-*/
-  //入金処理セッター
-  public void setBalance(int val1, int val2){
-    balance += val1;
+  //入金処理
+  public void inBalance(int val){
+    balance += val;
   }
 
-  //出金処理セッター
-  public void setBalanceOut(int val1, int val2){
-    balance -= val1;
+  //出金処理
+  public void outBalance(int val){
+    balance -= val;
   }
 
   //残高取得処理ゲッター
@@ -44,12 +29,12 @@ public class Deposit {
     return balance;
   }
   //口座番号取得処理ゲッター
-  public int getBankNum(){
+  public String getBankNum(){
     return bankNum;
   }
   //口座名取得処理ゲッター
-  // public string getBankName(){
-  //   return bankName;
-  // }
+  public String getBankName(){
+    return bankName;
+  }
 
 }
--- DepositMain.java
+++ DepositMain.java
@@ -1,12 +1,20 @@
 public class DepositMain {
   public static void main(String[] args) {
+    Deposit taro = new Deposit("0001", "太郎銀行口座", 10000);
+    Deposit jiro = new Deposit("0002", "次郎銀行口座", 10000);
     int val1 = Integer.parseInt(args[0]);
     int val2 = Integer.parseInt(args[1]);
-    Deposit depo = new Deposit();
-    depo.setBalance(val1, val2);
-    // System.out.println(depo.setBalanceOut(val1, val2));
-    System.out.println(depo.getBalance());
-    System.out.println(depo.getBankNum());
-    // System.out.println(depo.getBankName());
+    switch (args[2]) {
+      case "IN":
+        taro.inBalance(val1);
+        jiro.inBalance(val2);
+        break;
+      case "OUT":
+        taro.outBalance(val1);
+        jiro.outBalance(val2);
+        break;
+    }
+    System.out.println("口座名：" + taro.getBankName() + "（口座番号：" + taro.getBankNum() + "） 残高：" + taro.getBalance());
+    System.out.println("口座名：" + jiro.getBankName() + "（口座番号：" + jiro.getBankNum() + "） 残高：" + jiro.getBalance());
   }
 }
