591d605f9c7a29a1fb87fd3e02ccca6e912db117
uavpliyc <uavpliyc@gmail.com>
2021-08-21 12:55:12 +0900

    fix ChangeType

--- ChangeType.java
+++ ChangeType.java
@@ -1,18 +1,28 @@
 public class ChangeType {
   public static void main(String[] args){
-    int i = 10;
-    method1(i);
 
-    double d = method2();
+    int x = 10;
+    short y = (short)x;
+    System.out.println(y);
+
+    //キャスト
+    double a = 10.5;
+    int b = (int)a;
+    System.out.println(b);
+
+    double i = 10.5;
+    method1((int)i);
+
+    int d = method2();
     System.out.println(d);
   }
-  
-  public static void method1(double x){
-    System.out.println(x);
-  };
 
+  public static void method1(int x){
+    System.out.println(x);
+  }
+  
   public static int method2(){
-    int i = 10;
-    return i;
+    double i = 10.5;
+    return (int)i;
   }
 }
