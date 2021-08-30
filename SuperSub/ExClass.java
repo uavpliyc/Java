9c8844dce48824a043e14106f1e9d5ba58b77d9b
uavpliyc <uavpliyc@gmail.com>
2021-08-31 07:33:04 +0900

    add SuperSub package

--- SuperSub/ExClass.java
+++ SuperSub/ExClass.java
@@ -1,3 +1,4 @@
+package SuperSub;
 class ExClass {
   public void increase(int i) { // 引数を10倍にするメソッド
     int x = i * 10;
--- SuperSub/ExSubClassMain.java
+++ SuperSub/ExSubClassMain.java
@@ -1,3 +1,4 @@
+package SuperSub;
 public class ExSubClassMain {
   public static void main(String[] args){
     SubExClass sub = new SubExClass();
