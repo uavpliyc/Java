ce777a9740b459a880ad89aaa0a733a3896f6b32
uavpliyc <uavpliyc@gmail.com>
2021-09-03 07:57:08 +0900

    add generics

---
+++ generics.java
@@ -0,0 +1,11 @@
+public class generics<E> {
+  private E val;
+
+  public void setVal(E val){
+    this.val = val;
+  }
+
+  public E getVal(){
+    return val;
+  }
+}
---
+++ genericsMain.java
@@ -0,0 +1,8 @@
+public class genericsMain {
+  public static void main(String[] args) {
+    generics<String> gene = new generics<String>();
+
+    gene.setVal("sample");
+    System.out.println(gene.getVal());
+  }
+}
