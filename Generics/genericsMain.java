ab62c9496a430a7bdc963b6ebc7f381140390f58
uavpliyc <uavpliyc@gmail.com>
2021-09-16 07:29:20 +0900

    fix generics

--- Generics/genericsMain.java
+++ Generics/genericsMain.java
@@ -2,8 +2,11 @@ package Generics;
 public class genericsMain {
   public static void main(String[] args) {
     generics<String> gene = new generics<String>();
+    generics<Integer> gene1 = new generics<Integer>();
 
     gene.setVal("sample");
+    gene1.setVal(100);
     System.out.println(gene.getVal());
+    System.out.println(gene1.getVal());
   }
 }
