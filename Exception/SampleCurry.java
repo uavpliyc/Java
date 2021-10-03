f19af55c8cf8293ec9e32eda906dc2c53d4d5863
uavpliyc <uavpliyc@gmail.com>
2021-10-04 07:54:31 +0900

    fix samplecurry

--- Exception/SampleCurry.java
+++ Exception/SampleCurry.java
@@ -1,21 +1,18 @@
 package Exception;
-public class Sample {
+
+public class SampleCurry {
   public static void main(String[] args) {
 
     //Curryクラスのインスタンス化
     Curry curry = new Curry();
 
     //Curryクラスのフィールドの値を参照
-    // String name = curry.name;
     System.out.println(curry.name);
-
-    //Curryクラスのメソッドの実行
     System.out.println(curry.taste());
 
     CheeseCurry cheeseCurry = new CheeseCurry();
 
     System.out.println(cheeseCurry.newname);
-   
     System.out.println(cheeseCurry.newtaste());
 
   }
