586ddd4e6be2b7a22a68a911460911e710ef6690
uavpliyc <uavpliyc@gmail.com>
2021-10-09 08:16:25 +0900

    add stringmethod

---
+++ StringMethod.java
@@ -0,0 +1,6 @@
+public class StringMethod {
+  public static void main(String[] args) {
+    String str1 = "bronze,silver,gold";
+    System.out.println(str1.replaceAll("bronze", "silver"));
+  }
+}
