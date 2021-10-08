dcf7e6ca409935507b147a770cd70f9e8f3879ef
uavpliyc <uavpliyc@gmail.com>
2021-10-08 18:51:12 +0900

    add conccat

---
+++ ConcatTest.java
@@ -0,0 +1,10 @@
+public class ConcatTest {
+    public static void main(String[] arg){
+      String s1 = "X";
+      String s2 = s1 + "Y";
+      String s3 = s1.concat("Y");
+      System.out.println(s2 == s3);
+      System.out.println(s2);
+      System.out.println(s3);
+  }
+}
