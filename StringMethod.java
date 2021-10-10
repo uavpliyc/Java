2630598f3d37a79a593a8d657043203947a2f0c5
uavpliyc <uavpliyc@gmail.com>
2021-10-10 09:32:13 +0900

    fix stringmethod

--- StringMethod.java
+++ StringMethod.java
@@ -2,5 +2,7 @@ public class StringMethod {
   public static void main(String[] args) {
     String str1 = "bronze,silver,gold";
     System.out.println(str1.replaceAll("bronze", "silver"));
+    System.out.println(str1.indexOf("silver"));
+    System.out.println(str1);
   }
 }
