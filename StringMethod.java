0604cef016b402725e94097100904d9452621535
uavpliyc <uavpliyc@gmail.com>
2021-10-14 07:11:12 +0900

    add StringMethod trim

--- StringMethod.java
+++ StringMethod.java
@@ -7,8 +7,14 @@ public class StringMethod {
     System.out.println(str1.charAt(0));
 
     String s = "123.45";
-    int i = Integer.parseInt(s);
+    // int i = Integer.parseInt(s);
     //NumberFormatException
-    System.out.println(i);
+    // System.out.println(i);
+
+    String str2 = " te st ";
+    //文字列前後の空白を削除
+    str2 = str2.trim();
+    System.out.println(str2);
+    System.out.println(str2.indexOf(" "));
   }
 }
