46f33b974d400c33e389db5152de2e01cddabf9e
uavpliyc <uavpliyc@gmail.com>
2021-10-19 08:07:13 +0900

    fix Array

--- Array.java
+++ Array.java
@@ -1,6 +1,7 @@
 public class Array {
   public static void main(String[] args) {
     int[] array = { 10, 20, 30, 40, 50 };
+    System.out.println(array);
     for(int i = 0; i < array.length; i++){
       System.out.println(array[i]);
       System.out.println(array[i] * 1.1);
