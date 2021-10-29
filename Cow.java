9196685d35fbfdfc82f55c389bbbe8a9461462b6
uavpliyc <uavpliyc@gmail.com>
2021-10-30 08:36:42 +0900

    add Cow Animal

---
+++ Cow.java
@@ -0,0 +1,18 @@
+class Animal {
+  public static void say(){
+    System.out.println("Animal!");
+  }
+}
+
+public class Cow extends Animal {
+  // ※staticメソッドはオーバーライドされない！
+  public static void say(){
+    System.out.println("Moo!");
+  }
+  public static void main(String[] args) {
+    Animal[] animals = {new Animal(), new Cow()};
+    for (Animal a: animals){
+      a.say();
+    }
+  }
+}
