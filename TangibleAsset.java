2a8d47951a7109357829b0e20bdec96316f2d034
uavpliyc <uavpliyc@gmail.com>
2021-11-05 06:52:30 +0900

    add Asset

---
+++ TangibleAsset.java
@@ -0,0 +1,13 @@
+// 有形資産
+public abstract class TangibleAsset extends Asset{
+  String color;
+
+  public TangibleAsset(String name, int price, String color){
+    super(name, price);
+    this.color = color;
+  }
+
+  public String getColor(){
+    return this.color;
+  }
+}
