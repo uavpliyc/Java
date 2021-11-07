87f1cc8e566928fd56be24ca36172bc8f2832394
uavpliyc <uavpliyc@gmail.com>
2021-11-08 08:05:03 +0900

    add Exception

--- Exception/ExceptionTest.java
+++ Exception/ExceptionTest.java
@@ -2,24 +2,24 @@ package Exception;
 public class ExceptionTest {
   public static void main(String[] args) {
     Sample s = new Sample();
-
     try {
       s.runSample();
-
-      // 独自の例外をキャッチ
     } catch (ExceptionClass e) {
-      System.out.println(e.getMessage());// 独自の例外です
-      System.out.println(e);// test1.MyTest1Exception: 独自の例外です
+      // getMessageメソッド
+      System.out.println(e.getMessage());
+      System.out.println(e);
+      System.out.println("ーーーーーースタックトレースここからーーーーーーー");
+      // printStackTraceメソッド
+      e.printStackTrace();
+      System.out.println("ーーーーーースタックトレースここまでーーーーーーー");
     }
   }
 }
 
 class Sample {
   void runSample() throws ExceptionClass {
-
     int i = 5;
     if (i == 5) {
-
       // throwで作成した例外を投げる
       throw new ExceptionClass("独自の例外です");
     }
