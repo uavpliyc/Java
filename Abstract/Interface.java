14f1d02b8094b662607eb57ea1db4f53049bc70d
uavpliyc <uavpliyc@gmail.com>
2021-11-10 07:22:45 +0900

    add interface defaultMethod

--- Abstract/Interface.java
+++ Abstract/Interface.java
@@ -2,4 +2,7 @@ package Abstract;
 interface Interface {
   public static final int x = 10;
   public abstract void method();
+  default void foo(){
+    System.out.println("defaultMethod!");
+  }
 }
