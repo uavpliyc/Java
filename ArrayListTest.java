c28a03acdcd0d831e38862b9860f2014fb3217c9
uavpliyc <uavpliyc@gmail.com>
2021-11-16 20:32:33 +0900

    add ArrayList contains

--- ArrayListTest.java
+++ ArrayListTest.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class ArrayListTest {
   public static void main(final String[] args) {
-    
+
     ArrayList<Integer> list = new ArrayList<Integer>();
     list.add(1);
     list.add(2);
@@ -12,5 +12,8 @@ public class ArrayListTest {
 
     list.add(0, 10);
     System.out.println(list);
+
+    System.out.println(list.contains(1));
+    System.out.println(list.contains(5));
   }
 }
