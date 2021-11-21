1d128fbcc4606c791914c187221b2ec73804414c
uavpliyc <uavpliyc@gmail.com>
2021-11-22 07:52:33 +0900

    add StreamMethod

---
+++ StreamMethod.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class StreamMethod {
+  public static void main(String[] args) {
+    List<String> data1 = Arrays.asList("a", "b", "c");
+    // allMatchメソッド
+    boolean result1 = data1.stream().allMatch(s -> s == "a");
+    // anyMatchメソッド
+    boolean result2 = data1.stream().anyMatch(s -> s == "a");
+    // noneMatchメソッド
+    boolean result3 = data1.stream().noneMatch(s -> s == "a");
+    System.out.println(result1);
+    System.out.println(result2);
+    System.out.println(result3);
+
+    data1.forEach(System.out::println);
+
+    IntStream data2 = IntStream.of(10,20,30);
+    /**
+     * reduceメソッド
+     * @param1 初期値
+     * @param2 集約処理
+     */
+    int result4 = data2.reduce(0, (a,b) -> a + b);
+    System.out.println(result4);
+  }
+}
