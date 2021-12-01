6e866f8f1f618f30630e03ba9c51e1311c002948
uavpliyc <uavpliyc@gmail.com>
2021-12-02 08:25:02 +0900

    add Path methods

--- PathInterface.java
+++ PathInterface.java
@@ -5,6 +5,14 @@ public class PathInterface {
   public static void main(String[] args) {
     Path path = Paths.get("/Users/user/Desktop/test.txt");
     // toStringメソッド
-    System.out.format("toString:" + path.toString());
+    System.out.format("toString: %s%n" , path.toString());
+    // getFileNameメソッド
+    System.out.format("getFileName: %s%n" , path.getFileName());
+    // getNameメソッド：ルートパスから引数の数の要素を返す
+    System.out.format("getName: %s%n" , path.getName(0));
+    // getNameCountメソッド
+    System.out.format("getNameCount: %s%n" , path.getNameCount());
+    // getRootメソッド
+    System.out.format("getRoot: %s%n" , path.getRoot());
   }
 }
