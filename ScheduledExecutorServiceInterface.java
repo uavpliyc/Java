79a0801cb26cd7dc1ec39baae8e4db5e3bd882b0
uavpliyc <uavpliyc@gmail.com>
2021-12-07 08:06:41 +0900

    add scheduleWithFixedDelay method

--- ScheduledExecutorServiceInterface.java
+++ ScheduledExecutorServiceInterface.java
@@ -1,5 +1,4 @@
 import java.util.concurrent.Callable;
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -16,6 +15,20 @@ public class ScheduledExecutorServiceInterface {
 
       ScheduledFuture<?> result1 = service.schedule(task1, 3, TimeUnit.SECONDS);
       ScheduledFuture<Date> result2 = service.schedule(task2, 1, TimeUnit.MILLISECONDS);
+    } finally {
+      if (service != null) {
+        service.shutdown();
+      }
+    }
+
+    // scheduleWithFixedDelayメソッド：第3引数(秒)
+    try {
+      service = Executors.newSingleThreadScheduledExecutor();
+      Runnable task3 = () -> System.out.println(new Date());
+      service.scheduleWithFixedDelay(task3, 2, 2, TimeUnit.SECONDS);
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     } finally {
       if (service != null) {
         service.shutdown();
