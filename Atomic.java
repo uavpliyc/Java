17bfcde8803423962a90618a90f71e4f0a9a26f6
uavpliyc <uavpliyc@gmail.com>
2021-12-10 07:17:42 +0900

    add Atomic

---
+++ Atomic.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class Atomic {
+  public static void main(String[] args) {
+    AtomicInteger num1 = new AtomicInteger(0);
+    // int[] num2 = {0};
+    IntStream.iterate(1, i -> 1).limit(100).parallel().forEach(i -> num1.incrementAndGet());
+    System.out.println(num1);
+  }
+}
