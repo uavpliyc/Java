ece267588314cb02dd787625c6a2879e72ad4359
uavpliyc <uavpliyc@gmail.com>
2021-12-20 08:11:27 +0900

    add LocaleObject us

--- LocaleObject.java
+++ LocaleObject.java
@@ -3,6 +3,15 @@ import java.util.Locale;
 public class LocaleObject {
   public static void main(String[] args) {
     Locale japan = Locale.getDefault();
-    System.out.println(japan.getDisplayCountry() + ":" + japan.getDisplayLanguage());
+    System.out.println(japan.getDisplayCountry());
+    System.out.println(japan.getDisplayLanguage());
+
+    Locale us = Locale.US;
+
+    System.out.println(us.getDisplayCountry());
+    System.out.println(us.getDisplayLanguage());
+
+    System.out.println(us.getDisplayCountry(us));
+    System.out.println(us.getDisplayLanguage(us));
   }
 }
