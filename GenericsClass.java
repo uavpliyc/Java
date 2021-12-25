da8acdbe136a4d8d49a8e7aad34371c753d6420e
uavpliyc <uavpliyc@gmail.com>
2021-12-26 08:22:04 +0900

    fix GenericsClass

--- GenericsClass.java
+++ GenericsClass.java
@@ -1,13 +1,11 @@
 import java.util.List;
 
-public class GenericsClass {
-  public class Sample<T> {
+public class GenericsClass<T> {
 
     T t;
 
     List<T> method() {
       return null;
     }
-    
-  }
+
 }
