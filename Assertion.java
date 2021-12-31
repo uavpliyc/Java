2aca0c31a67233388bad24bc3627ca547c64bf09
uavpliyc <uavpliyc@gmail.com>
2021-12-31 13:05:19 +0900

    add Asssertion

---
+++ Assertion.java
@@ -0,0 +1,7 @@
+public class Assertion {
+  public static void main(String[] args) {
+    int price = Integer.parseInt(args[0]);
+    // 実行時は「java -ea」使用
+    assert (price > 0) : ("priceは0より大きくしてください");
+  }
+}
