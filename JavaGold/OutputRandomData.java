6c0b304b153d318eed984b9c5fddf6d05604e3a1
uavpliyc <uavpliyc@gmail.com>
2022-01-29 06:54:18 +0900

    add OutputRandomData

---
+++ JavaGold/OutputRandomData.java
@@ -0,0 +1,13 @@
+package JavaGold;
+
+import gold8.data.RandomData;
+
+public class OutputRandomData {
+
+  public static void main(String[] args) {
+
+    System.out.println(RandomData.getIndex(100));
+    System.out.println(RandomData.getIntList(0, 100, 1, 10));
+  }
+
+}
