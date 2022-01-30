571b1b911c9ea85cf5166500c254df3e1bd1cfec
uavpliyc <uavpliyc@gmail.com>
2022-01-31 08:24:53 +0900

    add People

--- JavaGold/OutputRandomData.java
+++ JavaGold/OutputRandomData.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 import gold8.data.RandomData;
 
@@ -18,8 +19,27 @@ public class OutputRandomData {
     System.out.println(RandomData.getIntList(0, 100, 1, 10)
     );
 
+    
     LocalDateTime now = LocalDateTime.now();
     String nowFormat = now.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    
+    People tanaka = new People(1,"田中","たなか","tanaka","男","1993/03/26",now.toString(),10000000,10000);
+
+    ArrayList<People> peopleList = new ArrayList<>();
+
+    peopleList.add(tanaka);
+
+    for(People list: peopleList){
+      System.out.println(list.getId());
+      System.out.println(list.getKanji());
+      System.out.println(list.getYomi());
+      System.out.println(list.getRomaji());
+      System.out.println(list.getSex());
+      System.out.println(list.getBirthday());
+      System.out.println(list.getRegisterDate());
+      System.out.println(list.getInsurance());
+      System.out.println(list.getPremium());
+    }
 
     try (
         FileOutputStream fos = new FileOutputStream(new File(Logging.getProperty("OutputDirectory") + "Contractor_" + nowFormat + ".csv"));
---
+++ JavaGold/People.java
@@ -0,0 +1,99 @@
+package JavaGold;
+
+public class People {
+
+  private int id;
+  private String kanji;
+  private String yomi;
+  private String romaji;
+  private String sex;
+  private String birthday;
+  private String registerDate;
+  private long insurance;
+  private int premium;
+
+  // コンストラクタ
+  public People(int id,String kanji,String yomi,String romaji,String sex,String birthday,String registerDate,long insurance,int premium) {
+    this.id = id;
+    this.kanji = kanji;
+    this.yomi = yomi;
+    this.sex = sex;
+    this.birthday = birthday;
+    this.registerDate = registerDate;
+    this.insurance = insurance;
+    this.premium = premium;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getKanji() {
+    return this.kanji;
+  }
+
+  public void setKanji(String kanji) {
+    this.kanji = kanji;
+  }
+
+  public String getYomi() {
+    return this.yomi;
+  }
+
+  public void setYomi(String yomi) {
+    this.yomi = yomi;
+  }
+
+  public String getRomaji() {
+    return this.romaji;
+  }
+
+  public void setRomaji(String romaji) {
+    this.romaji = romaji;
+  }
+
+  public String getSex() {
+    return this.sex;
+  }
+
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
+
+  public String getBirthday() {
+    return this.birthday;
+  }
+
+  public void setBirthday(String birthday) {
+    this.birthday = birthday;
+  }
+
+  public String getRegisterDate() {
+    return this.registerDate;
+  }
+
+  public void setRegisterDate(String registerDate) {
+    this.registerDate = registerDate;
+  }
+
+  public long getInsurance() {
+    return this.insurance;
+  }
+
+  public void setInsurance(long insurance) {
+    this.insurance = insurance;
+  }
+
+  public int getPremium() {
+    return this.premium;
+  }
+
+  public void setPremium(int premium) {
+    this.premium = premium;
+  }
+  
+}
