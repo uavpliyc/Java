b105b83ae3e0848fc6ae77a8b590f865c89fc03a
uavpliyc <uavpliyc@gmail.com>
2022-02-01 08:20:36 +0900

    fix FirstNameBean

--- JavaGold/FirstNameBean.java
+++ JavaGold/FirstNameBean.java
@@ -1,11 +1,23 @@
 package JavaGold;
 
-public class FirstNameBean {
+import gold8.bean.ConverterList;
+import gold8.bean.ConvertibleBean;
+
+public class FirstNameBean extends ConvertibleBean {
   
   private String Chinese;
   private String hiragana;
   private String English;
   private String sex;
+
+  // コンストラクタ
+  public FirstNameBean() {
+    super(new ConverterList());
+    addField(s -> Chinese = s, () -> Chinese);
+    addField(s -> hiragana = s, () -> hiragana);
+    addField(s -> English = s, () -> English);
+    addField(s -> sex = s, () -> sex);
+  }
 
   public String getChinese() {
     return this.Chinese;
