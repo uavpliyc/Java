50e50a224dadf05ad9d98aa61f6a1d5ec6cc646a
uavpliyc <uavpliyc@gmail.com>
2022-02-13 08:16:12 +0900

    add NumberFormat

--- Format.java
+++ Format.java
@@ -1,8 +1,16 @@
+import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Format {
   public static void main(String[] args) {
+    // NumberFormatオブジェクト取得
+    NumberFormat num = NumberFormat.getInstance();
+    // フォーマット化
+    String str = num.format(500.00);
+    System.out.println(str);
+
+
     DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yy,MM,dd");
     String target = "21,12,22";
     LocalDate date = LocalDate.parse(target, fmt);
