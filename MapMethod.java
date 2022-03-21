3437f766c5ea16dabda8045325801219ccc9b776
uavpliyc <uavpliyc@gmail.com>
2022-03-21 10:17:44 +0900

    add map merge

--- MapMethod.java
+++ MapMethod.java
@@ -6,7 +6,12 @@ public class MapMethod {
     Map<String, String> map1 = new HashMap<>();
     // put
     map1.put("A", "B");
-    // merge
+
+    /** mergeメソッド
+     * @param1:結合元キー
+     * @param2:結合する文字列
+     * @param3:結合方法
+     */
     map1.merge("A", "C", (v1,v2) -> v1.concat(v2));
     map1.merge("B", "C", (v1,v2) -> v1.concat(v2));
     System.out.println(map1);
