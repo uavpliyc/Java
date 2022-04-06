35e1e85bf75f7640654d95baf2264b56449ae3c0
uavpliyc <uavpliyc@gmail.com>
2022-04-07 08:16:19 +0900

    add CyclicBarrierTest

---
+++ CyclicBarrierTest.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierTest {
+  public static void main(String[] args) {
+
+    CyclicBarrier barrier = new CyclicBarrier(3);
+
+    Runnable r = () -> {
+      String threadName = Thread.currentThread().getName();
+      System.out.println(threadName + ": start");
+      try {
+        Thread.sleep((int)(Math.random() * 5000));
+        System.out.println(threadName + ": waiting...");
+        barrier.await();
+      } catch (InterruptedException | BrokenBarrierException e) {
+        e.printStackTrace();
+      }
+      System.out.println(threadName + ": end");
+    };
+
+    new Thread(r).start();
+    new Thread(r).start();
+    new Thread(r).start();
+
+  }
+}
