94e788c213c1870b139ee153ad7dd6af5bb5c09d
uavpliyc <uavpliyc@gmail.com>
2022-04-11 07:18:45 +0900

    add ComparableTest

---
+++ ComparableTest.java
@@ -0,0 +1,37 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ComparableTest {
+  public static void main(String[] args) {
+
+    Map<ComparableObj, String> map = new TreeMap<>();
+
+    map.put(new ComparableObj(1), "Gold");
+    map.put(new ComparableObj(3), "Bronze");
+    map.put(new ComparableObj(2), "Silver");
+
+    for(ComparableObj c : map.keySet()) {
+      System.out.println(c + ":" + map.get(c));
+    }
+
+  }
+}
+
+class ComparableObj implements Comparable<ComparableObj> {
+
+  private int rank;
+
+  public ComparableObj(int rank) {
+    this.rank = rank;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(this.rank);
+  }
+
+  @Override
+  public int compareTo(ComparableObj c) {
+    return this.rank - c.rank;
+  }
+}
