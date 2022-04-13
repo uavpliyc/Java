69dc1445830e34b49762a6f0be73a3e45356c7dc
uavpliyc <uavpliyc@gmail.com>
2022-04-14 08:13:35 +0900

    add Inner2

--- OuterTest.java
+++ OuterTest.java
@@ -1,11 +1,28 @@
 public class OuterTest {
-    public static void execute() {
-      OuterTest.Inner inner = new OuterTest().new Inner();
-      inner.doIt();
-    }
+
+  public static void execute() {
+    OuterTest.Inner inner = new OuterTest().new Inner();
+    inner.doIt();
+  }
+
   public class Inner {
     public void doIt() {
       System.out.println("JavaGold");
     }
   }
+
+  private int num = 100;
+
+  public void show() {
+    String str = "num:";
+    class Inner2 {
+      private int num = OuterTest.this.num;
+      public void show() {
+        num += 100;
+        System.out.println(str + num);
+      }
+    }
+    new Inner2().show();
+  }
+
 }
--- OuterTestMain.java
+++ OuterTestMain.java
@@ -1,5 +1,6 @@
 public class OuterTestMain {
   public static void main(String[] args) {
     OuterTest.execute();
+    new OuterTest().show();
   }
 }
