137917f37333c1d2f2e52ba67c771c73fb439de5
uavpliyc <uavpliyc@gmail.com>
2022-04-15 08:07:31 +0900

    add binarySearch

--- Sort.java
+++ Sort.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -10,6 +14,14 @@ public class Sort {
     set.add("A");
     set.add("D");
     System.out.println(set);
+
+    Comparator<Integer> obj = (v1, v2) -> v1 - v2;
+    List<Integer> list = Arrays.asList(10,2,30,2);
+    Collections.sort(list, obj);
+    // binarySearch：配列から検索した要素のインデックスを返す(要sort)
+    System.out.println(Collections.binarySearch(list, 2));
+    System.out.println(Collections.binarySearch(list, 30));
+    System.out.println(Collections.binarySearch(list, 100));
   }
 
 }
