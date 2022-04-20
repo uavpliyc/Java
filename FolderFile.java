522a1eb2557266c3a8317e5901733e95fbd7f1f5
uavpliyc <uavpliyc@gmail.com>
2022-04-21 07:41:42 +0900

    add DosFileAttributes

--- FolderFile.java
+++ FolderFile.java
@@ -1,13 +1,28 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.DosFileAttributes;
+
 public class FolderFile {
   public static void main(String[] args) {
-    
+
     FolderFile f = new FolderFile();
     System.out.println(f.concatPath("c:", "index.html"));
+
+    Path path = Paths.get("c:index.html");
+    try {
+      DosFileAttributes att = Files.readAttributes(path, DosFileAttributes.class);
+      System.out.println(att.isDirectory());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
+
   public String concatPath(String folder, String file){
     if (!folder.endsWith("¥¥")){
       folder += "¥";
     }
     return folder + file;
   }
+
 }
