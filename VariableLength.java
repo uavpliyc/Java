589ff7d51b5c89c1ba1fbea608931eceabcb5c89
uavpliyc <uavpliyc@gmail.com>
2022-04-30 09:15:03 +0900

    add VariableLength

---
+++ VariableLength.java
@@ -0,0 +1,22 @@
+public class VariableLength {
+  public static void main(String[] args) {
+
+    VaLe obj = new VaLe();
+    int[] ary = {10, 20, 30};
+    obj.method("1");
+    obj.method("2", 10);
+    obj.method("3", 10, 20);
+    obj.method("4", ary);
+
+  }
+}
+
+class VaLe {
+  // 可変長引数
+  public void method(String s, int... i){
+    System.out.println(s + "サイズ：" + i.length);
+    for(int i2 : i){
+      System.out.println("第2引数の値：" + i2);
+    }
+  }
+}
