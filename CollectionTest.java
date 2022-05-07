46b0728ef323259d5f3973973c760f824705428c
uavpliyc <uavpliyc@gmail.com>
2022-05-08 08:51:58 +0900

    add partitioningBy

--- CollectionTest.java
+++ CollectionTest.java
@@ -11,5 +11,10 @@ public class CollectionTest {
     Map<String, List<String>> map = st.collect(Collectors.groupingBy(s -> s.substring(0, 1)));
     System.out.println(map);
 
+    Stream<Integer> st2 = Stream.of(3,5,7,9);
+    // partitioningBy
+    Map<Boolean, List<Integer>> map2 = st2.collect(Collectors.partitioningBy(s -> s > 5));
+    System.out.println(map2);
+
   }
 }
