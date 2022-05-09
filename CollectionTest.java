6eb5f09018996dd1bfee4f22e45889aca69f1488
uavpliyc <uavpliyc@gmail.com>
2022-05-09 22:31:33 +0900

    add toSet

--- CollectionTest.java
+++ CollectionTest.java
@@ -16,5 +16,9 @@ public class CollectionTest {
     Map<Boolean, List<Integer>> map2 = st2.collect(Collectors.partitioningBy(s -> s > 5));
     System.out.println(map2);
 
+    Stream<Integer> st3 = Stream.of(1,5,2,9);
+    // toSet
+    System.out.println(st3.collect(Collectors.toSet()));
+
   }
 }
