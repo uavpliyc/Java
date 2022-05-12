f118258dcae2a427f6d35872e45c4259c942637a
uavpliyc <uavpliyc@gmail.com>
2022-05-13 08:30:26 +0900

    add PeriodTest

---
+++ PeriodTest.java
@@ -0,0 +1,10 @@
+import java.time.Period;
+
+public class PeriodTest {
+  public static void main(String[] args) {
+    
+    Period p1 = Period.ofYears(3);
+    System.out.println("ofYears：" + p1);
+
+  }
+}
