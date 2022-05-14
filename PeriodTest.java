b0de4fbe1dff34d1662a5eab60a3a288eee88b56
uavpliyc <uavpliyc@gmail.com>
2022-05-14 10:34:52 +0900

    add Period of

--- PeriodTest.java
+++ PeriodTest.java
@@ -2,9 +2,12 @@ import java.time.Period;
 
 public class PeriodTest {
   public static void main(String[] args) {
-    
+
     Period p1 = Period.ofYears(3);
     System.out.println("ofYears：" + p1);
+
+    Period p2 = Period.of(0, 10, 50);
+    System.out.println("of：" + p2);
 
   }
 }
