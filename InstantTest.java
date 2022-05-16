b6e6a0c7224521fc8bc09fc5ce1a1cbe48a27044
uavpliyc <uavpliyc@gmail.com>
2022-05-17 08:02:16 +0900

    add toInstant

--- InstantTest.java
+++ InstantTest.java
@@ -1,4 +1,6 @@
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 public class InstantTest {
@@ -7,11 +9,13 @@ public class InstantTest {
     // 1970-01-01 00:00:00
     System.out.println(Instant.EPOCH);
 
-    System.out.println(Instant.now());
-    
+    System.out.println("now:" + Instant.now());
+
     System.out.println(Instant.now().toEpochMilli());
 
+    // ZoneDateTimeなら引数不要、LocalDateTimeならZoneOffset引数が必要
     System.out.println(ZonedDateTime.now().toInstant());
+    System.out.println(LocalDateTime.now().toInstant(ZoneOffset.UTC));
 
     System.out.println(Instant.ofEpochSecond(0));
   }
