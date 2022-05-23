f867b924e51cd5f106953bbc038f50af6d317a46
uavpliyc <uavpliyc@gmail.com>
2022-05-23 09:49:15 +0900

    add Optional ofNullable

--- OptionalTest.java
+++ OptionalTest.java
@@ -5,10 +5,12 @@ public class OptionalTest {
 
     Optional<Integer> op1 = Optional.of(10);
     Optional<Integer> op2 = Optional.empty();
+    Optional<String> op3 = Optional.ofNullable("null");
 
     System.out.println("op1.get():" + op1.get());
     // emptyなら表示なし
     op2.ifPresent(System.out::println);
+    System.out.println(op3.get());
 
   }
 }
