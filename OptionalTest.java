43a8a10704b8f5621c391271bec905ec05c8265b
uavpliyc <uavpliyc@gmail.com>
2022-05-24 08:18:53 +0900

    add Optional isPresent

--- OptionalTest.java
+++ OptionalTest.java
@@ -10,7 +10,10 @@ public class OptionalTest {
     System.out.println("op1.get():" + op1.get());
     // emptyなら表示なし
     op2.ifPresent(System.out::println);
-    System.out.println(op3.get());
+    System.out.println("op3.get():" + op3.get());
+
+    // isPresent
+    System.out.println("op3.isPresent():" + op3.isPresent());
 
   }
 }
