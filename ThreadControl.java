2d7247b9ab145b41e9f6df3866b75a51dc901068
uavpliyc <uavpliyc@gmail.com>
2022-05-29 09:43:38 +0900

    add ThreadControl main

--- ThreadControl.java
+++ ThreadControl.java
@@ -1,6 +1,10 @@
 public class ThreadControl {
   public static void main(String[] args) {
-    
+    Share share = new Share();
+    Thread1 thread1 = new Thread1(share);
+    Thread2 thread2 = new Thread2(share);
+    thread1.start();
+    thread2.start();
   }
 }
 
