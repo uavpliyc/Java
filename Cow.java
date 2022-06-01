1c184fe917c76238b714de7623f2204bb1e3da9a
uavpliyc <uavpliyc@gmail.com>
2022-06-02 07:42:59 +0900

    fix Cow

--- Cow.java
+++ Cow.java
@@ -12,7 +12,8 @@ public class Cow extends Animal {
   public static void main(String[] args) {
     Animal[] animals = {new Animal(), new Cow()};
     for (Animal a: animals){
-      a.say();
+      say();
+      System.out.println(a);
     }
     say();
   }
