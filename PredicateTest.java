ce929e7d9b63a10c7cc050a315c92a8650edc402
uavpliyc <uavpliyc@gmail.com>
2022-07-01 07:10:05 +0900

    add PredicateTest

---
+++ PredicateTest.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateTest {
+  public static void main(String[] args) {
+    List<Integer> list = Arrays.asList(7,10,12,9,31);
+    Predicate<Integer> p = n -> {
+      int count = 0;
+      boolean result = n > 10;
+      System.out.println(count++ + ":");
+      return result;
+    };
+
+    list.stream().filter(p).findFirst().ifPresent(System.out::println);
+  }
+}
