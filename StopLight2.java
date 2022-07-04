72eb3c42fb8aa76a89438cbca8837ba1de01f8ee
uavpliyc <uavpliyc@gmail.com>
2022-07-05 08:00:13 +0900

    add getString

--- StopLight2.java
+++ StopLight2.java
@@ -1,10 +1,15 @@
 public enum StopLight2 {
   GREEN("緑"),RED("赤"),YELLOW("黄");
 
+  private String s;
+
   // コンストラクタ(publicだとコンパイルエラー)
   private StopLight2(String s) {
     this.s = s;
    }
 
-  private String s;
+   public String getString(){
+     return s;
+   }
+
 }
