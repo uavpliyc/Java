efdf27a9d36fc75bf0d2d78493244bdb8d39e9b7
uavpliyc <uavpliyc@gmail.com>
2022-07-08 07:18:59 +0900

    add Reference Foo

--- ReferenceMethod.java
+++ ReferenceMethod.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class ReferenceMethod {
   public static void main(String[] args) {
@@ -18,5 +19,17 @@ public class ReferenceMethod {
     List<Integer> list = Arrays.asList(1,2,3);
     // インスタンスメソッド参照
     list.forEach(System.out::println);
+
+    method(Foo::new);
+
+  }
+  static void method(Supplier<? extends Foo> foo){
+    foo.get().doIt();
+  }
+}
+
+class Foo{
+  public void doIt(){
+    System.out.println("DoIt");
   }
 }
