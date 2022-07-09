c2ebbc8b2df3a91109805ae078d6b6f4cb92ce7c
uavpliyc <uavpliyc@gmail.com>
2022-07-10 08:19:29 +0900

    add Concat comment

--- ConcatTest.java
+++ ConcatTest.java
@@ -3,8 +3,8 @@ public class ConcatTest {
       String s1 = "X";
       String s2 = s1 + "Y";
       String s3 = s1.concat("Y");
-      System.out.println(s2 == s3);
-      System.out.println(s2);
-      System.out.println(s3);
+      System.out.println("s2 == s3:" + s2 == s3);
+      System.out.println("s2:" + s2);
+      System.out.println("s3:" + s3);
   }
 }
