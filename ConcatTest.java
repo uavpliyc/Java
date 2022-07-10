a4cc328f5be636211e4f9e6057cce1df865bc246
uavpliyc <uavpliyc@gmail.com>
2022-07-11 08:31:24 +0900

    add Concat equals

--- ConcatTest.java
+++ ConcatTest.java
@@ -4,6 +4,7 @@ public class ConcatTest {
       String s2 = s1 + "Y";
       String s3 = s1.concat("Y");
       System.out.println("s2 == s3:" + s2 == s3);
+      System.out.println("s2.equals(s3):" + s2.equals(s3));
       System.out.println("s2:" + s2);
       System.out.println("s3:" + s3);
   }
