238a3b1ac49908c57555f6312ca77ef8a7c21763
uavpliyc <uavpliyc@gmail.com>
2022-07-13 08:20:56 +0900

    add Period comment

--- PeriodTest.java
+++ PeriodTest.java
@@ -5,12 +5,14 @@ import java.time.temporal.ChronoUnit;
 public class PeriodTest {
   public static void main(String[] args) {
 
+    // Period
     Period p1 = Period.ofYears(3);
     System.out.println("ofYears：" + p1);
 
     Period p2 = Period.of(0, 10, 50);
     System.out.println("of：" + p2);
 
+    // Duration
     Duration d1 = Duration.ofDays(1);
     System.out.println("ofDays：" + d1);
 
