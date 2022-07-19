bec450e22e2265d67f7b01d117a99b34f3512872
uavpliyc <uavpliyc@gmail.com>
2022-07-19 09:07:45 +0900

    add OuterTest2

--- OuterTestMain2.java
+++ OuterTestMain2.java
@@ -1,5 +1,20 @@
 public class OuterTestMain2 {
-  
+  public static void main(String[] args) {
+
+    OuterTest2.Inner1.doIt();
+
+    new OuterTest2(){
+      public void doSomething(){
+        class Inner2{
+          public void execute(){
+            System.out.println("doSomething");
+          }
+        }
+        new Inner2().execute();
+      }
+    }.doSomething();
+
+  }
 }
 
 
