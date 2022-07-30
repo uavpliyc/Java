01d4c2fdf59f509f91f15e55b1d9e60ba871ee2c
uavpliyc <uavpliyc@gmail.com>
2022-07-30 09:25:23 +0900

    add Duration HOURS

--- PeriodTest.java
+++ PeriodTest.java
@@ -20,6 +20,8 @@ public class PeriodTest {
     System.out.println("of：" + d2);
     Duration d3 = Duration.of(1, ChronoUnit.MILLIS);
     System.out.println("of：" + d3);
+    Duration d4 = Duration.of(1, ChronoUnit.HOURS);
+    System.out.println("of：" + d4);
 
   }
 }
