8a5f9eb659756e3d3fb0ea9f2438ee966ae19dbd
uavpliyc <uavpliyc@gmail.com>
2022-08-19 08:36:50 +0900

    add executeQuery

--- Jdbc.java
+++ Jdbc.java
@@ -11,7 +11,6 @@ public class Jdbc {
         Statement stmt = con.createStatement();
         ResultSet rs = stmt.executeQuery(sql);
         ) {
-          // executeQueryはnullを返さない(空のResultSetを返す)
           if (rs != null) System.out.println("rs != null!");
           if (rs.next()) {
             System.out.println(rs.getString(1));
@@ -20,8 +19,11 @@ public class Jdbc {
           boolean result1 = stmt.execute(sql);
           // executeUpdate：なければ0返す
           int result2 = stmt.executeUpdate(sql2);
+          // executeQuery：なければ空のResultSetを返す
+          ResultSet result3 = stmt.executeQuery(sql);
           System.out.println(result1);
           System.out.println(result2);
+          System.out.println(result3);
           // getMetaData
           System.out.println(rs.getMetaData());
         } catch (SQLException e) {
