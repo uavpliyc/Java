a3a35c63419c4110511d33038af8390d7f252507
uavpliyc <uavpliyc@gmail.com>
2022-09-28 08:41:36 +0900

    add AbstractClassA set

--- JavaGold/AbstractClassA.java
+++ JavaGold/AbstractClassA.java
@@ -1,5 +1,13 @@
 package JavaGold;
 
+import java.io.File;
+
 public abstract class AbstractClassA implements InterfaceB {
+
+  public void set(File file) {
+    if(file != null) {
+      
+    }
+  }
   
 }
