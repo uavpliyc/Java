5e55b1ad9e750087cb0296f752ad595532ef23b2
uavpliyc <uavpliyc@gmail.com>
2022-09-29 08:06:56 +0900

    add AbstractClassA set2

--- JavaGold/AbstractClassA.java
+++ JavaGold/AbstractClassA.java
@@ -1,12 +1,19 @@
 package JavaGold;
 
 import java.io.File;
+import java.nio.file.Path;
 
 public abstract class AbstractClassA implements InterfaceB {
 
   public void set(File file) {
     if(file != null) {
       
+    }
+  }
+
+  public void set2(Path path) {
+    if(path != null) {
+
     }
   }
   
