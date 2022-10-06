e74df7ec8087e8bbee9077bebda3f3587d2f732a
uavpliyc <uavpliyc@gmail.com>
2022-10-07 07:44:33 +0900

    fix AbstractClassB constractar

--- JavaGold/AbstractClassB.java
+++ JavaGold/AbstractClassB.java
@@ -2,12 +2,12 @@ package JavaGold;
 
 public abstract class AbstractClassB extends AbstractClassA {
   
-  public AbstractClassB(AbstractClassA[] aList) {
+  public AbstractClassB(InterfaceA[] aList) {
 
   }
 
   public void execute() {
-    
+    System.out.println();
   }
 
 }
