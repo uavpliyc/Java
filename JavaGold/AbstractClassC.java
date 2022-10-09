61b19ea5574d7ca0e592dcd93e633f002326df5a
uavpliyc <uavpliyc@gmail.com>
2022-10-09 09:21:32 +0900

    add AbstractClassC convert

--- JavaGold/AbstractClassC.java
+++ JavaGold/AbstractClassC.java
@@ -1,8 +1,16 @@
 package JavaGold;
 
+import gold8.bean.IOBean;
+
 public abstract class AbstractClassC extends AbstractClassA {
 
+  IOBean io;
+
   public AbstractClassC() {
+  }
+
+  protected IOBean convert() {
+    return io;
   }
 
 }
