03d95a87332477da845ba5fb0d77124114da6414
uavpliyc <uavpliyc@gmail.com>
2022-10-11 07:48:06 +0900

    add EnumOption constracter

--- JavaGold/EnumOption.java
+++ JavaGold/EnumOption.java
@@ -1,5 +1,17 @@
 package JavaGold;
 
 public enum EnumOption {
-  
+
+  FINISH("end");
+
+  private String op;
+
+  private EnumOption(String op) {
+    this.op = op;
+  }
+
+  public String getOp() {
+    return op;
+  }
+
 }
