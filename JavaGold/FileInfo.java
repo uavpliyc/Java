4ccbd7954e7ca4f23c2724697d4abc4b2e1a693c
uavpliyc <uavpliyc@gmail.com>
2022-10-14 08:01:13 +0900

    add FileInfo fileSystem

--- JavaGold/FileInfo.java
+++ JavaGold/FileInfo.java
@@ -1,5 +1,13 @@
 package JavaGold;
 
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class FileInfo {
+
+  FileSystem fileSystem = FileSystems.getDefault();
+
+  Path path = fileSystem.getPath("");
   
 }
