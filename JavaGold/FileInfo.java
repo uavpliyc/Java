b937dde135b9482292f743233ba7433b43e919a0
uavpliyc <uavpliyc@gmail.com>
2022-10-15 09:40:08 +0900

    add FileInfo getFileAttr

--- JavaGold/FileInfo.java
+++ JavaGold/FileInfo.java
@@ -1,7 +1,9 @@
 package JavaGold;
 
+import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileInfo {
@@ -9,5 +11,13 @@ public class FileInfo {
   FileSystem fileSystem = FileSystems.getDefault();
 
   Path path = fileSystem.getPath("");
-  
+
+  public void getFileAttr() {
+    try {
+      System.out.println(Files.readAttributes(path, "*"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
 }
