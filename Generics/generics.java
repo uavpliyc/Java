acd73181338203edf797f6a4d25b58afa7876946
uavpliyc <uavpliyc@gmail.com>
2022-11-03 12:00:13 +0900

    add generis static

--- Generics/generics.java
+++ Generics/generics.java
@@ -2,6 +2,9 @@ package Generics;
 public class generics<E> {
   private E val;
 
+  // クラス型パラメータをstaticメンバに使うと、コンパイルエラー
+  // private static E val2;
+
   public void setVal(E val){
     this.val = val;
   }
