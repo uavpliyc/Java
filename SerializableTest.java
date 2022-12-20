f6a725b0d2924f3fa2e656a05a56ca2ea7b7b79e
uavpliyc <uavpliyc@gmail.com>
2022-12-21 08:33:49 +0900

    add SerializableTest

---
+++ SerializableTest.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SerializableTest {
+    public static void main(String[] args) {
+        B b = new B();
+        try (
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test.txt"));
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test.txt"))
+        ) {
+            oos.writeObject(b);
+            System.out.println("シリアライズ完了");
+            B readB = (B)ois.readObject();
+            System.out.println("デシリアライズ完了");
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class A implements Serializable {
+    A() {
+        System.out.println("A");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("B");
+    }
+}
