43f2775a527b73e5d00741e2d2904cc0a7c671c3
uavpliyc <uavpliyc@gmail.com>
2022-12-24 10:48:21 +0900

    add FilesTest copy move

--- FilesTest.java
+++ FilesTest.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class FilesTest {
 public static void main(String[] args) throws IOException {
-    
+
 }
 
 
@@ -16,5 +18,15 @@ public static void main(String[] args) throws IOException {
         Files.deleteIfExists(Paths.get("nullDir"));
         
         // Files.deleteIfExists(Paths.get("tmp"));
+    }
+
+    void copyMove() throws IOException {
+        Path p1 = Paths.get("test1.txt");
+        Path p2 = Paths.get("test2.txt");
+        Path p3 = Paths.get("test3.txt");
+        Path p4 = Paths.get("test4.txt");
+    
+        Files.copy(p1, p2, StandardCopyOption.REPLACE_EXISTING);
+        Files.move(p3, p4, StandardCopyOption.REPLACE_EXISTING);
     }
 }
